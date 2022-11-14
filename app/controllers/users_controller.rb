ac62a4e18b60c7f9fadbe6ed5ad1ac3feaba3880
KumarAmitt <amitt2015@outlook.com>
2022-11-14 21:29:54 +0530

    Add about page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
 
@@ -16,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you successfully signed up"
       redirect_to articles_path
     else
@@ -24,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -41,6 +41,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root 'about#index'
+
   resources :articles
   
   get 'signup', to: 'users#new'
-  # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
