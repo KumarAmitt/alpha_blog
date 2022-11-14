6e62cf82cd7ca0af63d26155d0f179b4e429d9d4
KumarAmitt <amitt2015@outlook.com>
2022-11-14 23:54:30 +0530

    Add user deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def index
     @users = User.all
@@ -37,6 +39,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Account and all associated articles deleted successfully'
+    redirect_to articles_path
+  end
+
   private
 
   def user_params
@@ -45,6 +54,13 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+        flash[:alert] = 'You can only edit your own account'
+        redirect_to @user
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-  has_many :articles
+  has_many :articles, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
