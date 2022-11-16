f545b9b70ec809cb11fadd23ec80a7874d23983c
KumarAmitt <amitt2015@outlook.com>
2022-11-17 00:44:26 +0530

    Add views associated with manuplating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles
   end
 
   def new
@@ -21,6 +22,20 @@ class CategoriesController < ApplicationController
       redirect_to @category
     else
       render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit', status: :unprocessable_entity
     end
   end
 
